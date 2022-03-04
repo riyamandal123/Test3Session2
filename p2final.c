773386c9f58f1d600d2cc3d186e0c2f773654e28
Riya Mandal <96898236+riyamandal123@users.noreply.github.com>
2022-03-04 05:02:04 +0000

    p2final.c

--- p2final.c
+++ p2final.c
@@ -1,1 +1,47 @@
 #include<stdio.h>
+struct _fraction
+{
+  int num,den;
+};
+typedef _fraction fraction
+fraction;
+fraction input_fraction()
+{
+  fraction f;
+  printf("enter the fraction\n");
+  scanf("%d %d\n",&f);
+  return f;
+}
+fraction Largest_fraction(fraction f1, fraction f2, fraction f3)
+{
+ int sden = f1.den * f2.den * f3.den;
+  int a, b, c;
+  a = f1.num * sden/f1.den;
+  b = f2.num * sden/f2.den;
+  c = f3.num * sden/f3.den;
+  if( a>b && a>c)
+  {
+    return f1;
+  }
+  else if(b>c)
+  {
+    return f2;
+  }
+  else{
+    return f3;
+  }
+}
+void output(fraction f1, fraction f2, fraction f3, fraction largest)
+{
+  printf("the smallest of three fraction is %d/%d\n",largest.num,largest.den);
+}
+int main()
+{
+  fraction f1,f2,f3,largest;
+  f1=input_fraction();
+  f2=input_fraction();
+  f3=input_fraction();
+  largest=largest_fraction(f1,f2,f3);
+  output(largest);
+  return 0;
+}
