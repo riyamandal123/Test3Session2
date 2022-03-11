2fc82e471fb33cc975aad7b3d018ec4ea62fc91a
Riya Mandal <96898236+riyamandal123@users.noreply.github.com>
2022-03-11 03:42:50 +0000

    p4final.c

--- p4final.c
+++ p4final.c
@@ -1,45 +1,57 @@
-/*
-
-Write a program to find nth number in fibonacci sequence.
-Fibonacci sequence consists of 0,1,1,2,3,5,8,13,21........
-int input();
-int find_fibo(int n);
-void output(int n, int fibo);
-
-*/
-
-#include <stdio.h>
-
-int input()
+#include<stdio.h>
+int input_degree()
 {
-  int n;
-  printf("Enter the number\n");
-  scanf("%d",&n);
-  return n;
+    int n;
+    printf("Enter the degree of the polynomial\n");
+    scanf("%d",&n);
+    return n;
 }
-int find_fibo(int n)
+float input_x()
 {
-  int fibo=0;
-  int a=0;
-  int b=1;
-  for(int i=0;i<n;i++) {
-    fibo = a;
-    a = b;
-    b = fibo + b;
-  }
-  return fibo;
+    float x;
+    printf("Enter the value of x\n");
+    scanf("%f",&x); 
+    return x;       
 }
-
-
-void output(int n, int fibo)
+void input_coefficients(int n,float a[n])
 {
-  printf("the %dth fibonacci number is %d\n", n, fibo);
+    int i;
+    for( i=0;i<=n;i++)
+    {
+        printf("Enter the %d coefficients of x\n",i+1);
+        scanf("%f",&a[i]);
+    }
 }
-
+float evaluate_polynomial(int n, float a[n], float x)
+{
+    float temp1=0,temp2,result,temp3;
+    temp1=temp1+a[0];
+    for(int i=0;i<n;i++){
+        temp2=temp1*x;
+        temp3=a[i+1]+temp2;
+        temp1=temp3;
+    }
+    return temp1;
+}
+void output(int n, float a[n], float x, float result)
+{
+      printf("The value of ");
+    for(int i=0;i<n;i++)
+    {
+        printf("%.0f +",a[i]);
+    }
+    printf("%.0f",a[n]);
+    printf("is %.2f",result);
+}
 int main()
 {
-  int n = input();
-  int fibo = find_fibo(n);
-  output(n, fibo);
-  return 0;
+    int n;
+    float x,result;
+    n=input_degree();
+    x=input_x();
+    float a[n];
+    input_coefficients(n,a);
+    result=evaluate_polynomial(n,a,x);
+    output(n,a,x,result);
+    return 0;
 }
